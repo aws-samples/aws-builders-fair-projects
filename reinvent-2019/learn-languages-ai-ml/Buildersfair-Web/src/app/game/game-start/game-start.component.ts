c1d3966f350966778ab4b617c8f45eeb168beb67
soonki <soonki@a483e7bf21c6.ant.amazon.com>
2019-12-03 16:44:09 -0800

    Update game start UI and multi language support

--- reinvent-2019/learn-languages-ai-ml/Buildersfair-Web/src/app/game/game-start/game-start.component.ts
+++ reinvent-2019/learn-languages-ai-ml/Buildersfair-Web/src/app/game/game-start/game-start.component.ts
@@ -14,6 +14,7 @@ export class GameStartComponent implements OnInit {
   @Input() gameId: number;
   @Output() gameCreated = new EventEmitter<number>();
   gameForm: FormGroup;
+  languageCode: string;
 
   constructor(private http: HttpClient,
               private gameService: GameService,
@@ -30,13 +31,18 @@ export class GameStartComponent implements OnInit {
     });
   }
 
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterViewInit() {
+    document.getElementById('btnEnglish').click();
+  }
+
   createGame() {
 
     const username = document.getElementById('userName') as HTMLInputElement;
 
     const gameInfo = {
       user_name: username.value,
-      language_code: 'en-US'
+      language_code: this.languageCode
     };
 
     this.gameService.createGame(gameInfo).subscribe((newGameId: number) => {
@@ -46,6 +52,48 @@ export class GameStartComponent implements OnInit {
     }, error => {
       this.alertify.error(error);
     });
+  }
+
+  selectLanguage(languageCode: string) {
+    this.languageCode = languageCode;
+    // console.log(this.languageCode);
+
+    this.selectButton('btnEnglish', false);
+    this.selectButton('btnKorean', false);
+    this.selectButton('btnChinese', false);
+    this.selectButton('btnJapanese', false);
+    this.selectButton('btnSpanish', false);
+
+    switch (this.languageCode) {
+      case 'en-US':
+        this.selectButton('btnEnglish', true);
+        break;
+      case 'ko-KR':
+        this.selectButton('btnKorean', true);
+        break;
+      case 'cmn-CN':
+        this.selectButton('btnChinese', true);
+        break;
+      case 'ja-JP':
+        this.selectButton('btnJapanese', true);
+        break;
+      case 'es-ES':
+        this.selectButton('btnSpanish', true);
+        break;
+    }
+  }
+
+  selectButton(elementId: string, flag: boolean) {
+
+    const x = document.getElementById(elementId);
+
+    if (flag === false) {
+      x.classList.remove('btn-secondary');
+      x.classList.add('btn-light');
+    } else {
+      x.classList.remove('btn-light');
+      x.classList.add('btn-secondary');
+    }
   }
 
   gameStart(newGameId: number) {
